6806e728db69bda106f2fc5476da8a95cc172be7
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-06-01 15:06:57 +0200

    Added getting legions

--- app/page.js
+++ app/page.js
@@ -1,8 +1,39 @@
+"use client";
+
 import Image from "next/image";
 import styles from "@/styles/page.module.scss";
 import Title from "@/components/Title";
+import supabase from "@/utils/supabaseClient";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+	// Getting Legions
+	const [legions, setLegions] = useState([]);
+
+	// Loading State
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchLegions = async () => {
+			try {
+				const { data, error } = await supabase.from("legions").select("*");
+				if (error) {
+					console.error("Error fetching legions: ", error);
+				} else {
+					setLegions(data);
+				}
+			} catch (error) {
+				console.error("Unexpected error: ", error);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		fetchLegions();
+	}, []);
+
+	console.log(legions);
+
 	return (
 		<main className={styles.main}>
 			<Title />
