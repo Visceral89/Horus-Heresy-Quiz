921eaf094ba4213c325728c1bef39f0be33678bd
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-06-04 23:29:04 +0200

    starting on game logic

--- components/Game.jsx
+++ components/Game.jsx
@@ -2,8 +2,36 @@
 
 import { motion } from "framer-motion";
 import styles from "@/styles/game.module.scss";
+import { useState } from "react";
+import supabase from "@/utils/supabaseClient";
 
 const Game = () => {
+	const [quiz, setQuiz] = useState(false);
+	const [questions, setQuestions] = useState([]);
+	const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+	const startQuiz = () => {
+		setQuiz(true);
+	};
+
+	useEffect(() => {
+		const fetchQuestions = async () => {
+			try {
+				const { data, error } = await supabase.from("questions").select("*");
+
+				if (error) {
+					console.error("Error fetching questions: ", error);
+				} else {
+					setQuestions(data);
+				}
+			} catch (error) {
+				console.error("Unexpected error: ", error);
+			}
+		};
+
+		fetchQuestions();
+	}, []);
+
 	return (
 		<motion.div
 			className={styles.game}
@@ -11,7 +39,17 @@ const Game = () => {
 			animate={{ scale: 1 }}
 			exit={{ scale: 0 }}
 		>
-			Game
+			{!quiz ? (
+				<div className={styles.welcome}>
+					<p>
+						Welcome to the Horus Heresy Quiz! Are you ready to find out which
+						Space Marine Legion you belong to?
+					</p>
+					<button onClick={startQuiz}>Start Quiz</button>
+				</div>
+			) : (
+				<div>First Question</div>
+			)}
 		</motion.div>
 	);
 };
